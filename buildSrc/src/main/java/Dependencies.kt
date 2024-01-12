c672528ecbe54815a6e10efd5702fc55ce0514b3
Mike Penz <opensource@mikepenz.dev>
2024-01-12 10:53:11 +0100

    - update Dependencies - markdown 0.6.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,15 +5,14 @@ object Versions {
 
     const val kotlin = "1.9.21"
 
-    const val markdown = "0.6.0"
+    const val markdown = "0.6.1"
 
     const val coil = "2.5.0"
     const val compose = "1.5.4"
     const val composeCompiler = "1.5.5"
 
-    const val material = "1.10.0"
-    const val activityCompose = "1.8.1"
-    const val lifecycleKtx = "2.3.1"
+    const val material = "1.11.0"
+    const val activityCompose = "1.8.2"
 }
 
 object Deps {
