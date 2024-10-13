124e9cf8c319aa0982ef52c74464c1fc9fb6867a
Mike Penz <opensource@mikepenz.dev>
2024-10-13 09:10:09 +0200

    - simplify entity converter further

--- multiplatform-markdown-renderer/src/commonMain/kotlin/com/mikepenz/markdown/utils/EntityConverter.kt
+++ multiplatform-markdown-renderer/src/commonMain/kotlin/com/mikepenz/markdown/utils/EntityConverter.kt
@@ -1,42 +1,38 @@
 package com.mikepenz.markdown.utils
 
 import org.intellij.markdown.html.entities.Entities
-import kotlin.text.Regex
 
+/**
+ * Based on: https://github.com/JetBrains/markdown/blob/master/src/commonMain/kotlin/org/intellij/markdown/html/entities/EntityConverter.kt
+ * Removed HTML focused escaping by https://github.com/mikepenz/multiplatform-markdown-renderer/pull/222
+ */
 object EntityConverter {
-  private const val escapeAllowedString = """!"#\$%&'\(\)\*\+,\-.\/:;<=>\?@\[\\\]\^_`{\|}~"""
+    private const val escapeAllowedString = """!"#\$%&'\(\)\*\+,\-.\/:;<=>\?@\[\\\]\^_`{\|}~"""
+    private val REGEX = Regex("""&(?:([a-zA-Z0-9]+)|#([0-9]{1,8})|#[xX]([a-fA-F0-9]{1,8}));|(["&<>])""")
+    private val REGEX_ESCAPES = Regex("${REGEX.pattern}|\\\\([$escapeAllowedString])")
 
-  private val REGEX = Regex("""&(?:([a-zA-Z0-9]+)|#([0-9]{1,8})|#[xX]([a-fA-F0-9]{1,8}));|(["&<>])""")
-  private val REGEX_ESCAPES = Regex("${REGEX.pattern}|\\\\([$escapeAllowedString])")
-
-  fun replaceEntities(text: CharSequence, processEntities: Boolean, processEscapes: Boolean): String {
-    val regex = if (processEscapes) REGEX_ESCAPES else REGEX
-    return regex.replace(text) { match ->
-      val g = match.groups
-      when {
-        g.size > 5 && g[5] != null -> g[5]!!.value[0].toString()
-        g[4] != null -> match.value
-        else -> {
-          val code = when {
-            !processEntities -> {
-              null
+    fun replaceEntities(
+        text: CharSequence,
+        processEntities: Boolean,
+        processEscapes: Boolean
+    ): String {
+        val regex = if (processEscapes) REGEX_ESCAPES else REGEX
+        return regex.replace(text) { match ->
+            val g = match.groups
+            when {
+                g.size > 5 && g[5] != null -> g[5]!!.value[0].toString()
+                g[4] != null -> match.value
+                else -> {
+                    val code = when {
+                        !processEntities -> null
+                        g[1] != null -> Entities.map[match.value]
+                        g[2] != null -> g[2]!!.value.toInt()
+                        g[3] != null -> g[3]!!.value.toInt(16)
+                        else -> null
+                    }
+                    code?.toChar()?.toString() ?: "&${match.value.substring(1)}"
+                }
             }
-            g[1] != null -> {
-              Entities.map[match.value]
-            }
-            g[2] != null -> {
-              g[2]!!.value.toInt()
-            }
-            g[3] != null -> {
-              g[3]!!.value.toInt(16)
-            }
-            else -> {
-              null
-            }
-          }
-          code?.toChar()?.toString() ?: "&${match.value.substring(1)}"
         }
-      }
     }
-  }
 }
