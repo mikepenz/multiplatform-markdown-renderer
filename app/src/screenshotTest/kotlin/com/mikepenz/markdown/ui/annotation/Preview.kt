c8522add59077695e97ac12fa97004d0b14e579c
Mike Penz <opensource@mikepenz.dev>
2025-02-07 16:23:08 +0100

    - add new ElementsInListsTests to get more snapshot tests for various scenarios

---
+++ app/src/screenshotTest/kotlin/com/mikepenz/markdown/ui/annotation/Preview.kt
@@ -0,0 +1,9 @@
+package com.mikepenz.markdown.ui.annotation
+
+import android.content.res.Configuration
+import android.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true, backgroundColor = Color.WHITE.toLong(), heightDp = 300)
+@Preview(showBackground = true, backgroundColor = Color.BLACK.toLong(), heightDp = 300, uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class DarkLightPreview
