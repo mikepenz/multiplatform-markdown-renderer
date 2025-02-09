ced61cfede8b2c19114d1fd6c4dbb586b90347b6
Mike Penz <opensource@mikepenz.dev>
2025-02-09 08:42:47 +0100

    - some import optimisations

--- multiplatform-markdown-renderer/src/commonMain/kotlin/com/mikepenz/markdown/compose/elements/MarkdownDivider.kt
+++ multiplatform-markdown-renderer/src/commonMain/kotlin/com/mikepenz/markdown/compose/elements/MarkdownDivider.kt
@@ -1,7 +1,11 @@
 package com.mikepenz.markdown.compose.elements
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
