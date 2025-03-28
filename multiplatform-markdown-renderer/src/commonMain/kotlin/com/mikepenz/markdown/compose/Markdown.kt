68c9da6e5d3eb41e4ba91ea31efc5ae2b7f537ed
Mike Penz <opensource@mikepenz.dev>
2025-03-28 16:17:03 +0100

    - move markdown parsing into a producer and remember the state unless content or flavour changes

--- multiplatform-markdown-renderer/src/commonMain/kotlin/com/mikepenz/markdown/compose/Markdown.kt
+++ multiplatform-markdown-renderer/src/commonMain/kotlin/com/mikepenz/markdown/compose/Markdown.kt
@@ -1,5 +1,6 @@
 package com.mikepenz.markdown.compose
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import com.mikepenz.markdown.compose.components.MarkdownComponentModel
 import com.mikepenz.markdown.compose.components.MarkdownComponents
@@ -18,6 +22,7 @@ import com.mikepenz.markdown.model.MarkdownColors
 import com.mikepenz.markdown.model.MarkdownDimens
 import com.mikepenz.markdown.model.MarkdownExtendedSpans
 import com.mikepenz.markdown.model.MarkdownPadding
+import com.mikepenz.markdown.model.MarkdownResult
 import com.mikepenz.markdown.model.MarkdownTypography
 import com.mikepenz.markdown.model.NoOpImageTransformerImpl
 import com.mikepenz.markdown.model.ReferenceLinkHandlerImpl
@@ -51,6 +56,24 @@ import org.intellij.markdown.flavours.gfm.GFMElementTypes.TABLE
 import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
 import org.intellij.markdown.parser.MarkdownParser
 
+/**
+ * Renders the markdown content.
+ *
+ * @param content The markdown content to be rendered.
+ * @param colors The colors to be used for rendering.
+ * @param typography The typography to be used for rendering.
+ * @param modifier The modifier to be applied to the container.
+ * @param padding The padding to be applied to the container.
+ * @param dimens The dimensions to be used for rendering.
+ * @param flavour The flavour descriptor for parsing the markdown.
+ * @param imageTransformer The image transformer to be used for rendering images.
+ * @param annotator The annotator to be used for rendering annotations.
+ * @param extendedSpans The extended spans to be used for rendering.
+ * @param components The components to be used for rendering.
+ * @param animations The animations to be used for rendering.
+ * @param loading A composable function to be displayed while loading the content.
+ * @param error A composable function to be displayed in case of an error. Only really possible if assertions are enabled on the parser)
+ */
 @Composable
 fun Markdown(
     content: String,
@@ -65,6 +88,8 @@ fun Markdown(
     extendedSpans: MarkdownExtendedSpans = markdownExtendedSpans(),
     components: MarkdownComponents = markdownComponents(),
     animations: MarkdownAnimations = markdownAnimations(),
+    loading: @Composable (modifier: Modifier) -> Unit = { Box(modifier) {} },
+    error: @Composable (modifier: Modifier) -> Unit = { Box(modifier) {} },
 ) {
     CompositionLocalProvider(
         LocalReferenceLinkHandler provides ReferenceLinkHandlerImpl(),
@@ -78,13 +103,37 @@ fun Markdown(
         LocalMarkdownComponents provides components,
         LocalMarkdownAnimations provides animations,
     ) {
-        Column(modifier) {
-            val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(content)
-            parsedTree.children.forEach { node ->
-                if (!handleElement(node, components, content)) {
-                    node.children.forEach { child ->
-                        handleElement(child, components, content)
-                    }
+        val markdownResult by parseMarkdown(content = content, flavour = flavour)
+
+        val result = markdownResult
+        when (result) {
+            is MarkdownResult.Error -> error(modifier)
+            is MarkdownResult.Loading -> loading(modifier)
+            is MarkdownResult.Success -> MarkdownSuccess(content = content, node = result.result, components = components, modifier = modifier)
+        }
+    }
+}
+
+/**
+ * Renders the parsed markdown content.
+ *
+ * @param content The original markdown content.
+ * @param node The ASTNode representing the parsed markdown.
+ * @param components The MarkdownComponents instance containing the components to use.
+ */
+@Composable
+fun MarkdownSuccess(
+    content: String,
+    node: ASTNode,
+    components: MarkdownComponents,
+    modifier: Modifier = Modifier,
+) {
+    val skipLinkDefinition = false
+    Column(modifier) {
+        node.children.forEach { node ->
+            if (!handleElement(node, components, content, skipLinkDefinition)) {
+                node.children.forEach { child ->
+                    handleElement(child, components, content, skipLinkDefinition)
                 }
             }
         }
@@ -97,6 +146,7 @@ internal fun ColumnScope.handleElement(
     components: MarkdownComponents,
     content: String,
     includeSpacer: Boolean = true,
+    skipLinkDefinition: Boolean = true,
 ): Boolean {
     val model = MarkdownComponentModel(
         content = content,
@@ -123,7 +173,7 @@ internal fun ColumnScope.handleElement(
         ORDERED_LIST -> components.orderedList(this@handleElement, model)
         UNORDERED_LIST -> components.unorderedList(this@handleElement, model)
         IMAGE -> components.image(this@handleElement, model)
-        LINK_DEFINITION -> components.linkDefinition(this@handleElement, model)
+        LINK_DEFINITION -> if (!skipLinkDefinition) components.linkDefinition(this@handleElement, model)
         HORIZONTAL_RULE -> components.horizontalRule(this@handleElement, model)
         TABLE -> components.table(this@handleElement, model)
         else -> {
@@ -133,9 +183,55 @@ internal fun ColumnScope.handleElement(
 
     if (!handled) {
         node.children.forEach { child ->
-            handleElement(child, components, content, includeSpacer)
+            handleElement(child, components, content, includeSpacer, skipLinkDefinition)
         }
     }
 
     return handled
+}
+
+/**
+ * Handles the link definition node.
+ * This is a recursive function that traverses the AST tree and runs the link definition component.
+ *
+ * @param node The ASTNode representing the link definition.
+ * @param components The MarkdownComponents instance containing the components to use.
+ * @param content The original markdown content.
+ */
+@Composable
+internal fun ColumnScope.handleLinkDefinition(
+    node: ASTNode,
+    components: MarkdownComponents,
+    content: String,
+) {
+    val model = MarkdownComponentModel(
+        content = content,
+        node = node,
+        typography = LocalMarkdownTypography.current,
+    )
+    if (node.type == LINK_DEFINITION) {
+        components.linkDefinition(this@handleLinkDefinition, model)
+    } else {
+        node.children.forEach { child -> handleLinkDefinition(child, components, content) }
+    }
+}
+
+/**
+ * Parses the given markdown content and returns a [State] of [MarkdownResult].
+ */
+@Composable
+fun parseMarkdown(
+    content: String,
+    flavour: MarkdownFlavourDescriptor = GFMFlavourDescriptor(),
+): State<MarkdownResult> {
+    // Creates a State<T> with Result.Loading as initial value
+    // If content or flavour changes, the running producer will cancel and re-launch.
+    return produceState<MarkdownResult>(initialValue = MarkdownResult.Loading, content, flavour) {
+        // Update State with either an Error or Success result. This will trigger a recomposition where this State is read
+        value = try {
+            MarkdownResult.Success(MarkdownParser(flavour).buildMarkdownTreeFromString(content))
+        } catch (_: Throwable) {
+            MarkdownResult.Error
+        }
+    }
 }
