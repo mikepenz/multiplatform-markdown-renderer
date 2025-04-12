b2768d23637566de49439b4d060b10cddbb4b9c7
Mike Penz <opensource@mikepenz.dev>
2025-04-12 20:23:22 +0200

    - fix deprecation warning for aboutlibs in wasm sample

--- sample/web/src/commonMain/kotlin/com/mikepenz/markdown/Main.kt
+++ sample/web/src/commonMain/kotlin/com/mikepenz/markdown/Main.kt
@@ -3,7 +3,7 @@ package com.mikepenz.markdown
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
-import com.mikepenz.aboutlibraries.ui.compose.m3.rememberLibraries
+import com.mikepenz.aboutlibraries.ui.compose.rememberLibraries
 import com.mikepenz.markdown.sample.App
 import com.mikepenz.markdown.sample.web.resources.Res
 import org.jetbrains.compose.resources.ExperimentalResourceApi
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("baseLibs") {
-            from("com.mikepenz:version-catalog:0.2.7")
+            from("com.mikepenz:version-catalog:0.2.8")
         }
     }
 }
