b5bc6bf37fc279b56b6b4163e8ac2e20e73291ff
Mike Penz <opensource@mikepenz.dev>
2025-04-26 19:58:19 +0200

    - update to aboutlibraries 12.1.0-rc03

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("baseLibs") {
-            from("com.mikepenz:version-catalog:0.3.2")
+            from("com.mikepenz:version-catalog:0.3.3")
         }
     }
 }
