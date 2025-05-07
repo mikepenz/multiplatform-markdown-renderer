f11652d5a045afe362a2d848e7d583a565b247db
Mike Penz <opensource@mikepenz.dev>
2025-05-07 09:58:52 +0200

    - update to compose-multiplatform 1.8.0 - update to ktor 3.1.3 - update to aboutlibraries 12.1.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("baseLibs") {
-            from("com.mikepenz:version-catalog:0.3.3")
+            from("com.mikepenz:version-catalog:0.3.5")
         }
     }
 }
