5132756340ce369035c225932baa265cd0adb0b0
Mike Penz <opensource@mikepenz.dev>
2025-07-04 11:09:47 +0200

    - upgrade to Kotlin 2.2 - upgrade to compose 1.8.3 and compose multiplatform 1.8.2 - coil 3.2.0 - ktor 3.2.1 - dependency updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("baseLibs") {
-            from("com.mikepenz:version-catalog:0.4.2")
+            from("com.mikepenz:version-catalog:0.6.0")
         }
     }
 }
