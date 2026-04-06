158b6bf78752c8a56eefc510053fc58a07d7acf8
Mike Penz <opensource@mikepenz.dev>
2026-04-06 12:42:55 +0200

    chore(deps): update dependencies
    via com.mikepenz:version-catalog 0.13.1 -> 0.14.1:
    - paparazzi app.cash.paparazzi:2.0.0-alpha04 -> dev.mikepenz.paparazzi:0.0.1-a02

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("baseLibs") {
-            from("com.mikepenz:version-catalog:0.13.1")
+            from("com.mikepenz:version-catalog:0.14.1")
         }
     }
 }
